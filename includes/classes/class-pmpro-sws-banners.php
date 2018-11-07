09962c07f446a09660a47c596be11058895fef0c
Paul Barthmaier <paul@barthmaier.rocks>
2018-11-07 13:55:43 -0500

    initial commit

--- includes/classes/class-pmpro-sws-banners.php
+++ includes/classes/class-pmpro-sws-banners.php
@@ -2,14 +2,18 @@
 namespace PMPro_Sitewide_Sales\includes\classes;
 
 defined( 'ABSPATH' ) || die( 'File cannot be accessed directly' );
-
 /**
  * Handles registering banners and displaying banners on frontend.
+ *
+ * @package pmpro-sitewide-sales
+ * @author  Paid Memberships Pro <info@paidmembershipspro.com>
+ * @license   GPL-2.0+
  */
 class PMPro_SWS_Banners {
-
 	/**
-	 * Adds actions
+	 * [init] Method to initiate actions.
+	 *
+	 * @return void
 	 */
 	public static function init() {
 		add_action( 'wp', array( __CLASS__, 'choose_banner' ) );
@@ -17,16 +21,19 @@ class PMPro_SWS_Banners {
 	}
 
 	/**
-	 * Gets info about available banners including name and available
-	 * css selectors.
+	 * [get_registered_banners] Gets info about available banners including name and available css selectors.
 	 *
-	 * @return array banner_name => array( option_title=>string, callback=>string, css_selctors=>array(strings) )
+	 * @return array
+	 *
+	 * banner_name => array(
+	 * option_title=>string, callback=>string, css_selctors=>array(strings)
+	 * )
 	 */
 	public static function get_registered_banners() {
 
 		$registered_banners = array(
-			'top' => array(
-				'option_title'  => __( 'Yes, Top of Site', 'pmpro_sitewide_Sales' ),
+			'top'          => array(
+				'option_title'  => __( 'Yes, Top of Site', 'pmpro-sitewide-sales' ),
 				'callback'      => array( __CLASS__, 'hook_top_banner' ),
 				'css_selectors' => array(
 					'.pmpro_sws_banner',
@@ -35,8 +42,8 @@ class PMPro_SWS_Banners {
 					'#pmpro_sws_banner_top .pmpro_btn',
 				),
 			),
-			'bottom' => array(
-				'option_title'  => __( 'Yes, Bottom of Site', 'pmpro_sitewide_sales' ),
+			'bottom'       => array(
+				'option_title'  => __( 'Yes, Bottom of Site', 'pmpro-sitewide-sales' ),
 				'callback'      => array( __CLASS__, 'hook_bottom_banner' ),
 				'css_selectors' => array(
 					'.pmpro_sws_banner',
@@ -50,7 +57,7 @@ class PMPro_SWS_Banners {
 				),
 			),
 			'bottom_right' => array(
-				'option_title'  => __( 'Yes, Bottom Right of Site', 'pmpro_sitewide_sales' ),
+				'option_title'  => __( 'Yes, Bottom Right of Site', 'pmpro-sitewide-sales' ),
 				'callback'      => array( __CLASS__, 'hook_bottom_right_banner' ),
 				'css_selectors' => array(
 					'.pmpro_sws_banner',
@@ -75,7 +82,9 @@ class PMPro_SWS_Banners {
 	}
 
 	/**
-	 * Logic for when to show banners/which banner to show
+	 * [choose_banner] Logic for when to show banners/which banner to show.
+	 *
+	 * @return [type] [description]
 	 */
 	public static function choose_banner() {
 		global $pmpro_pages;
@@ -86,7 +95,7 @@ class PMPro_SWS_Banners {
 		}
 
 		// get some settings
-		$options = PMPro_SWS_Settings::get_options();
+		$options              = PMPro_SWS_Settings::get_options();
 		$active_sitewide_sale = $options['active_sitewide_sale_id'];
 		$membership_level     = pmpro_getMembershipLevelForUser();
 
@@ -94,7 +103,7 @@ class PMPro_SWS_Banners {
 		$preview = false;
 		if ( current_user_can( 'administrator' ) && isset( $_REQUEST['pmpro_sws_preview_sale_banner'] ) ) {
 			$active_sitewide_sale = intval( $_REQUEST['pmpro_sws_preview_sale_banner'] );
-			$preview = true;
+			$preview              = true;
 		}
 
 		// unless we are previewing, don't show the banner on certain pages
@@ -106,19 +115,19 @@ class PMPro_SWS_Banners {
 
 			// no discount code
 			$discount_code_id = get_post_meta( $active_sitewide_sale, 'pmpro_sws_discount_code_id', true );
-			if( empty( $discount_code_id ) || $discount_code_id < 0 ) {
+			if ( empty( $discount_code_id ) || $discount_code_id < 0 ) {
 				return;
 			}
 
 			// no landing page or on it
 			$landing_page_post_id = get_post_meta( $active_sitewide_sale, 'pmpro_sws_landing_page_post_id', true );
-			if( empty( $landing_page_post_id ) || $landing_page_post_id < 0 || is_page( $landing_page_post_id ) ) {
+			if ( empty( $landing_page_post_id ) || $landing_page_post_id < 0 || is_page( $landing_page_post_id ) ) {
 				return;
 			}
 
 			// use banner set to false
 			$use_banner = get_post_meta( $active_sitewide_sale, 'pmpro_sws_use_banner', true );
-			if( empty( $use_banner ) || 'no' === $use_banner ) {
+			if ( empty( $use_banner ) || 'no' === $use_banner ) {
 				return;
 			}
 
@@ -129,22 +138,22 @@ class PMPro_SWS_Banners {
 
 			// don't show on checkout page if set that way
 			$hide_on_checkout = get_post_meta( $active_sitewide_sale, 'pmpro_sws_hide_on_checkout', true );
-			if( $hide_on_checkout && is_page( $pmpro_pages['checkout'] ) ) {
+			if ( $hide_on_checkout && is_page( $pmpro_pages['checkout'] ) ) {
 				return;
 			}
 
 			// don't show banner to users of certain Levels
 			$hide_for_levels = get_post_meta( $active_sitewide_sale, 'pmpro_sws_hide_for_levels', true );
-			if( !empty( $hide_for_levels ) && !empty( $membership_level )
+			if ( ! empty( $hide_for_levels ) && ! empty( $membership_level )
 				&& in_array( $membership_level->ID, $hide_for_levels ) ) {
 				return;
 			}
 
 			// hide before/after the start/end dates
 			$start_date = get_post_meta( $active_sitewide_sale, 'pmpro_sws_start_date', true );
-			$end_date = get_post_meta( $active_sitewide_sale, 'pmpro_sws_end_date', true );
-			$today = date( 'Y-m-d', current_time( 'timestamp') );
-			if( $today < $start_date || $today >= $end_date ) {
+			$end_date   = get_post_meta( $active_sitewide_sale, 'pmpro_sws_end_date', true );
+			$today      = date( 'Y-m-d', current_time( 'timestamp' ) );
+			if ( $today < $start_date || $today >= $end_date ) {
 				return;
 			}
 		}
@@ -152,7 +161,7 @@ class PMPro_SWS_Banners {
 		// Display the appropriate banner
 		// get_post_meta( $active_sitewide_sale, 'use_banner', true ) will be something like top, bottom, etc.
 		$registered_banners = self::get_registered_banners();
-		$banner_to_use = get_post_meta( $active_sitewide_sale, 'pmpro_sws_use_banner', true );
+		$banner_to_use      = get_post_meta( $active_sitewide_sale, 'pmpro_sws_use_banner', true );
 		if ( current_user_can( 'administrator' ) && isset( $_REQUEST['pmpro_sws_preview_banner_type'] ) ) {
 			$banner_to_use = $_REQUEST['pmpro_sws_preview_banner_type'];
 		}
@@ -171,10 +180,12 @@ class PMPro_SWS_Banners {
 	}
 
 	/**
-	 * Applies user's custom css to banner
+	 * [apply_custom_css] Applies user's custom css to banner.
+	 *
+	 * @return string
 	 */
 	public static function apply_custom_css() {
-		$options = PMPro_SWS_Settings::get_options();
+		$options              = PMPro_SWS_Settings::get_options();
 		$active_sitewide_sale = $options['active_sitewide_sale_id'];
 		if ( current_user_can( 'administrator' ) && isset( $_REQUEST['pmpro_sws_preview_sale_banner'] ) ) {
 			$active_sitewide_sale = $_REQUEST['pmpro_sws_preview_sale_banner'];
@@ -187,27 +198,33 @@ class PMPro_SWS_Banners {
 		$css = get_post_meta( $active_sitewide_sale, 'pmpro_sws_css_option', true )
 		?>
 		<!--Sitewide Sale Add On for Paid Memberships Pro Custom CSS-->
-		<style type="text/css"><?php
-			if ( ! empty( $css ) ) {
-				echo $css;
-			}
-		?></style>
+		<style type="text/css">
+		<?php
+		if ( ! empty( $css ) ) {
+			echo $css;
+		}
+		?>
+		</style>
 		<!--/Sitewide Sale Add On for Paid Memberships Pro Custom CSS-->
 		<?php
 	}
 
 	/**
-	 * Sets top banner to be added
+	 * [hook_top_banner] Sets top banner to be added.
+	 *
+	 * @return void
 	 */
 	public static function hook_top_banner() {
 		add_action( 'wp_head', array( __CLASS__, 'show_top_banner' ) );
 	}
 
 	/**
-	 * Adds top banner
+	 * [show_top_banner] Adds top banner.
+	 *
+	 * @return [type] [description]
 	 */
 	public static function show_top_banner() {
-		$options = PMPro_SWS_Settings::get_options();
+		$options              = PMPro_SWS_Settings::get_options();
 		$active_sitewide_sale = $options['active_sitewide_sale_id'];
 		if ( current_user_can( 'administrator' ) && isset( $_REQUEST['pmpro_sws_preview_sale_banner'] ) ) {
 			$active_sitewide_sale = $_REQUEST['pmpro_sws_preview_sale_banner'];
@@ -220,35 +237,45 @@ class PMPro_SWS_Banners {
 				$banner_template = false;
 			}
 		}
-		/* Maybe use JavaScript here to detect the height of the bar and adjust margin-top of html elemenet. */
+
+		// Maybe use JavaScript here to detect the height of the bar and adjust margin-top of html elemenet.
 		?>
-		<div id="pmpro_sws_banner_top" class="pmpro_sws_banner<?php if ( ! empty( $banner_template ) ) { echo ' pmpro_sws_banner_template-' . esc_html( $banner_template ); } ?>">
+		<div id="pmpro_sws_banner_top" class="pmpro_sws_banner
+		<?php
+		if ( ! empty( $banner_template ) ) {
+			echo ' pmpro_sws_banner_template-' . esc_html( $banner_template ); }
+		?>
+		">
 			<div class="pmpro_sws_banner-inner">
-				<h3><?php _e( get_post_meta( $active_sitewide_sale, 'pmpro_sws_banner_title', true ) ); ?></h3>
+				<h3><?php esc_html_e( get_post_meta( $active_sitewide_sale, 'pmpro_sws_banner_title', true ) ); ?></h3>
 				<?php echo esc_attr_e( get_post_field( 'post_content', $active_sitewide_sale ) ); ?>
-				<span class="pmpro_sws_banner-button"><a class="pmpro_btn" href="<?php echo get_permalink( get_post_meta( $active_sitewide_sale, 'pmpro_sws_landing_page_post_id', true ) ); ?>"><?php _e( get_post_meta( $active_sitewide_sale, 'pmpro_sws_link_text', true ) ); ?></a></span>
+				<span class="pmpro_sws_banner-button"><a class="pmpro_btn" href="<?php echo esc_url( get_permalink( get_post_meta( $active_sitewide_sale, 'pmpro_sws_landing_page_post_id', true ) ) ); ?>"><?php esc_html_e( get_post_meta( $active_sitewide_sale, 'pmpro_sws_link_text', true ), 'pmpro-sitewide-sales' ); ?></a></span>
 			</div>
 		</div> <!-- end pmpro_sws_banner -->
 		<?php
 	}
 
 	/**
-	 * Sets bottom banner to be added
+	 * [hook_bottom_banner] Sets bottom banner to be added.
+	 *
+	 * @return void
 	 */
 	public static function hook_bottom_banner() {
 		add_action( 'wp_footer', array( __CLASS__, 'show_bottom_banner' ) );
 	}
 
 	/**
-	 * Adds bottom banner
+	 * [show_bottom_banner] Creates the bottom banner.
+	 *
+	 * @return void Adds banner to page.
 	 */
 	public static function show_bottom_banner() {
-		$options = PMPro_SWS_Settings::get_options();
+		$options              = PMPro_SWS_Settings::get_options();
 		$active_sitewide_sale = $options['active_sitewide_sale_id'];
 		if ( current_user_can( 'administrator' ) && isset( $_REQUEST['pmpro_sws_preview_sale_banner'] ) ) {
 			$active_sitewide_sale = $_REQUEST['pmpro_sws_preview_sale_banner'];
 		}
-		
+
 		// Display the wrapping div for selected template.
 		if ( defined( 'MEMBERLITE_VERSION' ) || ( pmpro_getOption( 'pmpro_sws_allow_template' ) === 'Yes' ) ) {
 			$banner_template = esc_html( get_post_meta( $active_sitewide_sale, 'pmpro_sws_banner_template', true ) );
@@ -257,15 +284,20 @@ class PMPro_SWS_Banners {
 			}
 		}
 		?>
-		<div id="pmpro_sws_banner_bottom" class="pmpro_sws_banner<?php if ( ! empty( $banner_template ) ) { echo ' pmpro_sws_banner_template-' . esc_html( $banner_template ); } ?>">
+		<div id="pmpro_sws_banner_bottom" class="pmpro_sws_banner
+		<?php
+		if ( ! empty( $banner_template ) ) {
+			echo ' pmpro_sws_banner_template-' . esc_html( $banner_template ); }
+		?>
+		">
 			<div class="pmpro_sws_banner-inner">
 			<a href="javascript:void(0);" onclick="document.getElementById('pmpro_sws_banner_bottom').style.display = 'none';" class="dismiss" title="Dismiss"></a>
 				<div class="pmpro_sws_banner-inner-left">
-					<h3><?php _e( get_post_meta( $active_sitewide_sale, 'pmpro_sws_banner_title', true ) ); ?></h3>
+					<h3><?php esc_html_e( get_post_meta( $active_sitewide_sale, 'pmpro_sws_banner_title', true ), 'pmpro-sitewide-sales' ); ?></h3>
 					<?php echo apply_filters( 'the_content', get_post_field( 'post_content', $active_sitewide_sale ) ); ?>
 				</div>
 				<div class="pmpro_sws_banner-inner-right">
-					<span class="pmpro_sws_banner-button"><a class="pmpro_btn" href="<?php echo get_permalink( get_post_meta( $active_sitewide_sale, 'landing_page_post_id', true ) ); ?>"><?php _e( get_post_meta( $active_sitewide_sale, 'pmpro_sws_link_text', true ) ); ?></a></span>
+					<span class="pmpro_sws_banner-button"><a class="pmpro_btn" href="<?php echo get_permalink( get_post_meta( $active_sitewide_sale, 'landing_page_post_id', true ) ); ?>"><?php esc_html_e( get_post_meta( $active_sitewide_sale, 'pmpro_sws_link_text', true ), 'pmpro-sitewide-sales' ); ?></a></span>
 				</div>
 			</div> <!-- end pmpro_sws_banner-inner -->
 		</div> <!-- end pmpro_sws_banner -->
@@ -273,17 +305,21 @@ class PMPro_SWS_Banners {
 	}
 
 	/**
-	 * Sets bottom right banner to be added
+	 * [hook_bottom_right_banner] Sets bottom right banner to be added.
+	 *
+	 * @return void
 	 */
 	public static function hook_bottom_right_banner() {
 		add_action( 'wp_footer', array( __CLASS__, 'show_bottom_right_banner' ) );
 	}
 
 	/**
-	 * Adds bottom right banner
+	 * [show_bottom_right_banner] Creates the bottom right banner.
+	 *
+	 * @return void Adds banner to page.
 	 */
 	public static function show_bottom_right_banner() {
-		$options = PMPro_SWS_Settings::get_options();
+		$options              = PMPro_SWS_Settings::get_options();
 		$active_sitewide_sale = $options['active_sitewide_sale_id'];
 		if ( current_user_can( 'administrator' ) && isset( $_REQUEST['pmpro_sws_preview_sale_banner'] ) ) {
 			$active_sitewide_sale = $_REQUEST['pmpro_sws_preview_sale_banner'];
@@ -297,13 +333,18 @@ class PMPro_SWS_Banners {
 			}
 		}
 		?>
-		<div id="pmpro_sws_banner_bottom_right" class="pmpro_sws_banner<?php if ( ! empty( $banner_template ) ) { echo ' pmpro_sws_banner_template-' . esc_html( $banner_template ); } ?>">
+		<div id="pmpro_sws_banner_bottom_right" class="pmpro_sws_banner
+		<?php
+		if ( ! empty( $banner_template ) ) {
+			echo ' pmpro_sws_banner_template-' . esc_html( $banner_template ); }
+		?>
+		">
 			<div class="pmpro_sws_banner-inner">
 				<a href="javascript:void(0);" onclick="document.getElementById('pmpro_sws_banner_bottom_right').style.display = 'none';" class="dismiss" title="Dismiss"></a>
-				<h3><?php _e( get_post_meta( $active_sitewide_sale, 'pmpro_sws_banner_title', true ) ); ?></h3>
+				<h3><?php esc_html_e( get_post_meta( $active_sitewide_sale, 'pmpro_sws_banner_title', true ), 'pmpro-sitewide-sales' ); ?></h3>
 				<?php echo wpautop( get_post_field( 'post_content', $active_sitewide_sale ) ); ?>
 			</div> <!-- end pmpro_sws_banner-inner -->
-			<span class="pmpro_sws_banner-button"><a class="pmpro_btn" href="<?php echo get_permalink( get_post_meta( $active_sitewide_sale, 'pmpro_sws_landing_page_post_id', true ) ); ?>"><?php _e( get_post_meta( $active_sitewide_sale, 'pmpro_sws_link_text', true ) ); ?></a></span>
+			<span class="pmpro_sws_banner-button"><a class="pmpro_btn" href="<?php echo esc_url( get_permalink( get_post_meta( $active_sitewide_sale, 'pmpro_sws_landing_page_post_id', true ) ) ); ?>"><?php esc_html_e( get_post_meta( $active_sitewide_sale, 'pmpro_sws_link_text', true ), 'pmpro-sitewide-sales' ); ?></a></span>
 		</div> <!-- end pmpro_sws_banner -->
 		<?php
 	}
